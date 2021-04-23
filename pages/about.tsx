8cc89347d1cb812bbadd199871b795e19cedc13b
K <K@gmail.com>
2021-04-24 01:28:52 +0800

    chore: test

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,8 @@
-// import React from 'react';
+import React from 'react';
 import Link from 'next/link';
 import Layout from '../components/Layout';
 
-const AboutPage = () => (
+const AboutPage = (): JSX.Element => (
     <Layout title="About | Next.js + TypeScript Example">
         <h1>About</h1>
         <p>This is the about page</p>
