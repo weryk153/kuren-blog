877650a39cfa35dfb5fcb1fc79660fd85ca8a812
K <K@gmail.com>
2021-04-25 22:37:28 +0800

    feature: create footer、header

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = (): JSX.Element => (
+    <footer>
+        <hr />
+        <span>我是 footer</span>
+    </footer>
+);
+
+export default Footer;
---
+++ components/Header.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Header = (): JSX.Element => (
+    <header>
+        <nav>
+            <Link href="/">
+                <a>Home</a>
+            </Link>{' '}
+            |{' '}
+            <Link href="/about">
+                <a>About</a>
+            </Link>{' '}
+            |{' '}
+            <Link href="/users">
+                <a>Users List</a>
+            </Link>{' '}
+            | <a href="/api/users">Users API</a>
+        </nav>
+    </header>
+);
+
+export default Header;
