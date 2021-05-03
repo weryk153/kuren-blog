788bd5b9a6ce441d9c8d8178514fe0e8d4b2347e
K <K@gmail.com>
2021-05-04 01:03:17 +0800

    feature: modify code

--- utils/mdxUtils.ts
+++ utils/mdxUtils.ts
@@ -1,12 +1,12 @@
-import fs from 'fs';
+const fs = require('fs');
 // import glob from 'fast-glob'
-import path from 'path';
+const path = require('path');
 
 // POSTS_PATH is useful when you want to get the path to a specific file
 export const POSTS_PATH = path.join(process.cwd(), 'pages/post');
 
 // postFilePaths is the list of all mdx files inside the POSTS_PATH directory
-export const blogFilePaths = fs.readdirSync(POSTS_PATH).map((path) => {
+export const blogFilePaths = fs.readdirSync(POSTS_PATH).map((path: string) => {
     return path.replace('.mdx', '');
 })
 // export const blogFilePaths = glob.sync(`**/*.mdx`, { absolute: false });
